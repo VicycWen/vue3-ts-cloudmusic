e408af1c96563cd3f5b2009f8bf5da23fcf1c9fa
温远超 <nfcwyc@163.com>
2023-09-16 01:54:25 +0800

    feat: add vant tabs

---
+++ src/components/Content.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <van-tabs class="tabs" v-model:active="active">
+      <van-tab title="推荐音乐">推荐音乐</van-tab>
+      <van-tab title="热歌榜">热歌榜</van-tab>
+      <van-tab title="搜索">搜索</van-tab>
+    </van-tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import { Tab, Tabs } from 'vant'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    [Tab.name]: Tab,
+    [Tabs.name]: Tabs
+  },
+  setup() {
+    return {
+        active: 0
+    }
+  }
+})
+</script>
+
+<style scoped>
+.tabs {
+    --van-tab-font-size: 15px;
+    --van-tab-active-text-color: #dd001b;
+    --van-font-bold: 400;
+    --van-tabs-bottom-bar-color: #dd001b;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
         <Header></Header>
+        <Content></Content>
     </div>
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
+import Content from '@/components/Content.vue';
 </script>
 
 <style scoped>
