805373710fb5bf1662ffd40ab26bd116f4d55e5d
温远超 <nfcwyc@163.com>
2023-09-16 01:18:39 +0800

    feat: add header.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Header></Header>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Header from '@/components/Header.vue';
+</script>
+
+<style scoped>
+
+</style>
